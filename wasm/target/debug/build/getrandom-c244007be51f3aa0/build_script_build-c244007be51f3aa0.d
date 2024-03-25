697a99fcca452779d5c5cd6689424b9eae108ca3
Ruslan Yun <leonemsolis@gmail.com>
2024-03-25 13:29:13 +0500

    add wasm frontend project

---
+++ wasm/target/debug/build/getrandom-c244007be51f3aa0/build_script_build-c244007be51f3aa0.d
@@ -0,0 +1,5 @@
+/Users/ls/dev/rust_projects/chip8/wasm/target/debug/build/getrandom-c244007be51f3aa0/build_script_build-c244007be51f3aa0: /Users/ls/.cargo/registry/src/index.crates.io-6f17d22bba15001f/getrandom-0.1.16/build.rs
+
+/Users/ls/dev/rust_projects/chip8/wasm/target/debug/build/getrandom-c244007be51f3aa0/build_script_build-c244007be51f3aa0.d: /Users/ls/.cargo/registry/src/index.crates.io-6f17d22bba15001f/getrandom-0.1.16/build.rs
+
+/Users/ls/.cargo/registry/src/index.crates.io-6f17d22bba15001f/getrandom-0.1.16/build.rs:
---
+++ wasm/target/debug/build/libc-6a5b897df520db0e/build_script_build-6a5b897df520db0e.d
@@ -0,0 +1,5 @@
+/Users/ls/dev/rust_projects/chip8/wasm/target/debug/build/libc-6a5b897df520db0e/build_script_build-6a5b897df520db0e: /Users/ls/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.153/build.rs
+
+/Users/ls/dev/rust_projects/chip8/wasm/target/debug/build/libc-6a5b897df520db0e/build_script_build-6a5b897df520db0e.d: /Users/ls/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.153/build.rs
+
+/Users/ls/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.153/build.rs:
